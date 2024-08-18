959f8026196b6f614096f3de9af4361747d5b797
solidcyber1 <solidcyber1@hotmail.com>
2024-08-18 19:19:21 +0200

    CreateProductCommandHandler

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,List<string> Category, string Description, string ImageFile, decimal Price)
+    :IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult> {
+    Task<CreateProductResult> IRequestHandler<CreateProductCommand, CreateProductResult>.Handle(CreateProductCommand request, CancellationToken cancellationToken) {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
+
